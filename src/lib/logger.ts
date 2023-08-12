222b94481caaffecf9910ffe207bb963fc122ef0
Nicolai <nicolaibernhardchristensen@gmail.com>
2023-08-13 00:35:46 +0200

    add error logger and modify info with prefix aswell

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,6 +1,9 @@
 import chalk from 'chalk';
 export const logger = {
   info(...args: unknown[]) {
-    console.log(chalk.blue(...args));
+    console.log(chalk.blue('INFO:', ...args));
+  },
+  error(...args: unknown[]) {
+    console.log(chalk.red('ERROR:', ...args));
   },
 };
