8949f5271bde9435865b0766b30aaf679677669a
Nicolai <nicolaibernhardchristensen@gmail.com>
2023-08-13 17:48:28 +0200

    just fethc the damn txt file

--- src/lib/render-title.ts
+++ src/lib/render-title.ts
@@ -9,7 +9,9 @@ const poimandres_theme = {
   yellow: '#fffac2',
 };
 
-const create_drippy_api = await fs.readFile('./src/lib/ascii.txt', 'utf-8');
+const create_drippy_api = await fetch(
+  'https://gist.githubusercontent.com/barrybtw/224c6345f069463b849dc9acc33e4a08/raw/14e36aec99e4fdd353b75ea5e2a0ad09770afeb1/ascii.txt',
+).then((res) => res.text());
 
 export const render_title = () => {
   const gradient_string = gradient(Object.values(poimandres_theme));
